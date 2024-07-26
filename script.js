d1d076bdb68763a537dc9efbbfe4d169182b1f3b
vaibha987 <v2goyal95@gmail.com>
2024-07-26 18:52:52 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,7 @@
-//your JS code here. If required.
+function strip(bandName) {
+  return bandName.replace(/^(a |the |an )/i, '').trim();
+}
+
+let sortedBands = bands.sort((a, b) => strip(a) > strip(b) ? 1 : -1);
+document.querySelector('#bands').innerHTML =
+  sortedBands.map(band => `<li>${band}</li>`).join('');
