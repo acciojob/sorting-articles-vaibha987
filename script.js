963aa6f3256f3c403e58b1d54a142f31cf24e49b
vaibha987 <v2goyal95@gmail.com>
2024-08-04 13:29:50 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,11 +1,12 @@
 const bands = ['The Plot in You', 'The Devil Wears Prada', 'Pierce the Veil', 'Norma Jean', 'The Bled', 'Say Anything', 'The Midway State', 'We Came as Romans', 'Counterparts', 'Oh, Sleeper', 'A Skylit Drive', 'Anywhere But Here', 'An Old Dog'];
-
-document.addEventListener('DOMContentLoaded', (event) => {
-  // Your code here
-	function strip(bandName) {
-  return bandName.replace(/^(a |the |an )/i, '').trim();
-}
-	let sortedBands = bands.sort((a, b) => strip(a) > strip(b) ? 1 : -1);
-document.querySelector('#bands').innerHTML =
-  sortedBands.map(band => `<li>${band}</li>`).join('');
+let list=document.getElementById("band")
+bands.sort((a, b) => {
+  let bandA = a.replace(/^(a |the |an )/i, '').trim();
+  let bandB = b.replace(/^(a |the |an )/i, '').trim();
+  return bandA < bandB ? -1 : 1;
+});
+bands.forEach(band => {
+  let li = document.createElement("li");
+  li.innerText = band;
+  list.appendChild(li);
 });
