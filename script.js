1c22f38ba30a097146846c1567cfa57778352f11
vaibha987 <v2goyal95@gmail.com>
2024-08-04 13:32:41 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 const bands = ['The Plot in You', 'The Devil Wears Prada', 'Pierce the Veil', 'Norma Jean', 'The Bled', 'Say Anything', 'The Midway State', 'We Came as Romans', 'Counterparts', 'Oh, Sleeper', 'A Skylit Drive', 'Anywhere But Here', 'An Old Dog'];
-let list=document.getElementById("band")
+let list=document.getElementById("bands")
 bands.sort((a, b) => {
   let bandA = a.replace(/^(a |the |an )/i, '').trim();
   let bandB = b.replace(/^(a |the |an )/i, '').trim();
