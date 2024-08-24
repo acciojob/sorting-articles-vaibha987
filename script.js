02838402103797c865409efbe8ca1d05909d6ea3
vaibha987 <v2goyal95@gmail.com>
2024-08-24 20:19:59 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,8 +1,8 @@
 const bands = ['The Plot in You', 'The Devil Wears Prada', 'Pierce the Veil', 'Norma Jean', 'The Bled', 'Say Anything', 'The Midway State', 'We Came as Romans', 'Counterparts', 'Oh, Sleeper', 'A Skylit Drive', 'Anywhere But Here', 'An Old Dog'];
 let list=document.getElementById("bands")
 bands.sort((a, b) => {
-  let bandA = a.replace(/(^a|the|an)/i, '').trim();
-  let bandB = b.replace(/(^a|the|an)/i, '').trim();
+  let bandA = a.replace(/(^a | the | an)/i, '').trim();
+  let bandB = b.replace(/(^a | the | an)/i, '').trim();
   return bandA < bandB ? -1 : 1;
 });
 bands.forEach(band => {
